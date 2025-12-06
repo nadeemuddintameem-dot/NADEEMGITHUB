8718b1f19030732a7ee6d503577fd7cbe74707b6
nadeemuddintameem-dot <nadeemuddintameem@gmail.com>
2025-12-06 18:17:41 +0530

    Add files via upload
    Initial upload

---
+++ chat.js
@@ -0,0 +1,14 @@
+function sendMessage() {
+  let input = document.getElementById("userInput").value;
+  if (!input) return;
+
+  const chat = document.getElementById("chatbox");
+  chat.innerHTML += "<p><b>You:</b> " + input + "</p>";
+
+  // Simple reply
+  let reply = "Thanks for your message: " + input;
+
+  chat.innerHTML += "<p><b>Bot:</b> " + reply + "</p>";
+
+  document.getElementById("userInput").value = "";
+}
