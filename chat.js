8a48fb5510aba5087abe3834a7fa4541cce98a6c
nadeemuddintameem-dot <nadeemuddintameem@gmail.com>
2025-12-06 19:12:32 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,10 +2,9 @@ function sendMessage() {
   let input = document.getElementById("userInput").value;
   if (!input) return;
 
-  const chat = document.getElementById("chatbox");
+  const chat = document.getElementById("chatBox");
   chat.innerHTML += "<p><b>You:</b> " + input + "</p>";
 
-  // Simple reply
   let reply = "Thanks for your message: " + input;
 
   chat.innerHTML += "<p><b>Bot:</b> " + reply + "</p>";
